90033a20f304946591e020b20e2b546739b514b7
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-10-31 14:36:33 +0000

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,13 +1,19 @@
-    function handleKeyDown(event) {
-      if (event.key === "Enter" && !event.shiftKey) {
-        event.preventDefault();
-        showResult();
-      }
-    }
-    function showResult() {
-      var code = document.getElementById("code-area").innerText;
-      var resultWindow = window.open("", "_blank");
-      resultWindow.document.open();
-      resultWindow.document.write("<html><head></head><body>" + code + "</body></html>");
-      resultWindow.document.close();
-    }
+function handleKeyDown(event) {
+  const codeArea = document.getElementById("code-area");
+  
+  if (event.key === "Enter" && !event.shiftKey) {
+    event.preventDefault();
+    document.execCommand("insertText", false, "\n");
+  } else if (event.key === "Enter" && event.shiftKey) {
+    event.preventDefault();
+    showResult();
+  }
+}
+
+function showResult() {
+  var code = document.getElementById("code-area").innerText;
+  var resultWindow = window.open("", "_blank");
+  resultWindow.document.open();
+  resultWindow.document.write("<html><head></head><body>" + code + "</body></html>");
+  resultWindow.document.close();
+}
